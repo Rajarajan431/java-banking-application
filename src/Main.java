fcdeb9634ec66ee694e97888a288a917eb00a755
Rajarajan <rajarajancps@gmail.com>
2026-08-16 22:20:34 +0530

    Add account registration input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== ACCOUNT REGISTRATION ====");
+
+        System.out.println("Enter account holder name: ");
+        String holderName = sc.nextLine();
+
+        System.out.println("Enter the account number: ");
+        int accountNum = sc.nextInt();
+
+        System.out.println("Enter initial balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.println("=== ACCOUNT DETAILS ====");
+        System.out.println("Name: " + holderName);
+        System.out.println("Account Number: " + accountNum);
+        System.out.println("Initial Balance: " + balance);
+
     }
 }
